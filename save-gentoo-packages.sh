6386e59b6101183eced98227c80b2b40ebeecf23
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-12-30 12:00:23 -0500

    Consolidate package saving scripts

--- save-gentoo-packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-REPO="${HOME}/gentoo-vm"
-WORLD=/var/lib/portage/world
-DEST="${REPO}${WORLD}"
-
-mkdir -p $(dirname "$DEST")
-cp "$WORLD" "$DEST"
---
+++ save-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+command-exists() {
+	command -v "$1" >& /dev/null
+}
+
+main() {
+	if command-exists emerge; then  # Gentoo
+		REPO="${HOME}/gentoo-vm"
+		WORLD=/var/lib/portage/world
+		DEST="${REPO}${WORLD}"
+		mkdir -p $(dirname "$DEST")
+		cp "$WORLD" "$DEST"
+	elif command-exists pkg_info; then  # OpenBSD
+		pkg_info -mz > openbsd-packages.txt
+	fi
+}
+
+main
